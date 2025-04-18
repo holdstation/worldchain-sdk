0d35320f75ed1cc8aed198ec7c3c1b6f845fe525
hieu <65597836+hmh10098@users.noreply.github.com>
2025-04-18 15:17:44 +0700

    fix listners

--- src/history/manager.ts
+++ src/history/manager.ts
@@ -66,6 +66,8 @@ export class Manager {
           if (runner) {
             runner.stop();
           }
+
+          delete this.listeners[address.toLowerCase()];
         },
       };
     } finally {
